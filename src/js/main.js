eaa1f77678cdbf513d0e61b68c6acb863e82155b
jzm <148263295+j-zm@users.noreply.github.com>
2024-10-27 21:16:59 +0100

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+const footerYear = document.querySelector(".footer__year");
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+handleCurrentYear();
